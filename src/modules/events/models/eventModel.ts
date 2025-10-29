42554fffc8055e150a084f36906f054d729b3fe4
Rolf Sparv <hrothi.wulfaz@gmail.com>
2025-10-29 22:13:01 +0100

    first draft

---
+++ src/modules/events/models/eventModel.ts
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+
+const groupSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    organiser: {
+        type: mongoose.Types.ObjectId,
+        ref: "UserAccount",
+        required: true,
+    },
+    place: {
+        type: String,
+    },
+    groups: [{
+        type: mongoose.Types.ObjectId,
+        ref: "UserGroup",
+    }],
+    startDate: {
+        type: Date,
+        min: Date.now() + 1000 * 60 * 60 * 24,
+        required: true,
+    },
+    endDate: {
+        type: Date,
+        min: Date.now() + 1000 * 60 * 60 * 48,
+    },
+    subEvents: [{
+        type: mongoose.Types.ObjectId
+    }],
+});
+
+export default mongoose.model("Event", groupSchema);
