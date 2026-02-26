2e12be6d4570de8d49a8fc4698082d7e4aebfeac
Rolf Sparv <hrothi.wulfaz@gmail.com>
2026-02-27 00:01:02 +0100

    added 400s and fallback 500 to error middleware

--- src/utils/errorsToHttp.ts
+++ src/utils/errorsToHttp.ts
@@ -23,4 +23,37 @@ export const interpretErrorsHttp = (err: unknown) => {
     if (err instanceof Errors.SubeventNotFoundError) {
         return errorReturner(404, err.message);
     };
+
+    if (err instanceof Errors.JoiValidationError) {
+        return errorReturner(400, err.message);
+    };
+    if (err instanceof Errors.ObjectIdValidationError) {
+        return errorReturner(400, err.message);
+    };
+    if (err instanceof Errors.MissingObjectIdError) {
+        return errorReturner(400, err.message);
+    };
+    if (err instanceof Errors.MissingGroupNameError) {
+        return errorReturner(400, err.message);
+    };
+    if (err instanceof Errors.MissingEmailError) {
+        return errorReturner(400, err.message);
+    };
+    if (err instanceof Errors.InvalidEmailFormatError) {
+        return errorReturner(400, err.message);
+    };
+    if (err instanceof Errors.MissingVerificationTokenError) {
+        return errorReturner(400, err.message);
+    };
+    if (err instanceof Errors.MissingRefreshTokenError) {
+        return errorReturner(400, err.message);
+    };
+    if (err instanceof Errors.MissingPasswordResetTokenError) {
+        return errorReturner(400, err.message);
+    };
+    if (err instanceof Errors.MissingNewPasswordError) {
+        return errorReturner(400, err.message);
+    };
+
+    return errorReturner(500, "Internal server error");
 };
