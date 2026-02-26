ad56131a6f1e22facf8875f39ee4c3bdae42e15e
Rolf Sparv <hrothi.wulfaz@gmail.com>
2026-02-26 23:44:59 +0100

    added 404s to error middleware

--- src/utils/errorsToHttp.ts
+++ src/utils/errorsToHttp.ts
@@ -1,29 +1,26 @@
 import * as Errors from "../errors/errors";
 
+const errorReturner = (code: number, msg: string) => {
+    return {
+        status: code,
+        body: {
+            success: false,
+            message: msg
+        }
+    }
+};
+
 export const interpretErrorsHttp = (err: unknown) => {
     if (err instanceof Errors.GroupNotFoundError) {
-        return {
-            status: 404,
-            body: { success: false, message: err.message }
-        };
+        return errorReturner(404, err.message);
     };
-
-    if (err instanceof Errors.JoiValidationError) {
-        return {
-            status: 400,
-            body: { success: false, message: err.message}
-        };
+    if (err instanceof Errors.UserNotFoundError) {
+        return errorReturner(404, err.message);
     };
-
-    if (err instanceof Errors.InvalidEmailFormatError) {
-        return {
-            status: 400,
-            body: { success: false, message: err.message }
-        };
+    if (err instanceof Errors.EventNotFoundError) {
+        return errorReturner(404, err.message);
     };
-
-    return {
-        status: 500,
-        body: { success: false, message: "Server error" }
+    if (err instanceof Errors.SubeventNotFoundError) {
+        return errorReturner(404, err.message);
     };
 };
